cfbbab4ae3e081e12a9c70460f2b8b0cf5d156b7
202007055 <abc50050@gmail.com>
2022-01-16 11:17:24 +0900

    주석 추가

--- Ref_Shooting_Game/Ref_Shooting_Game/GameHandler.cpp
+++ Ref_Shooting_Game/Ref_Shooting_Game/GameHandler.cpp
@@ -81,31 +81,27 @@ void GameHandler::SethWnd(HWND nhWnd)
     hWnd = nhWnd;
 }
 
-
-
-
-
-
+// 움직이는거 쓰레드로 구현
 DWORD __stdcall GameHandler::test(LPVOID param)
 {
     GameHandler* play = GetInstance();
     PlayerBase* player = play->player;
-
+    
     while (1)
-    {   
+    {   // 해당 키가 눌리면 0x8000을 반환함 해당 키들을 계속 확인하면서 키가 눌렸는지 확인함
         if (GetKeyState(0x57) & 0x8000) //w
         {
-            if (player->GetLocation().y >= 3 )
+            if (player->GetLocation().y >= 10 )
             {
-                player->SetLocation(POINT{ player->GetLocation().x, player->GetLocation().y - 3 });
+                player->SetLocation(POINT{ player->GetLocation().x, player->GetLocation().y - 10 });
                 InvalidateRect(hWnd, NULL, FALSE);
             }
         }
         if (GetKeyState(0x41) & 0x8000) //a
         {
-            if (player->GetLocation().x >= 3)
+            if (player->GetLocation().x >= 10)
             {
-                player->SetLocation(POINT{ player->GetLocation().x - 3, player->GetLocation().y });
+                player->SetLocation(POINT{ player->GetLocation().x - 10, player->GetLocation().y });
                 InvalidateRect(hWnd, NULL, FALSE);
             }
         }
@@ -113,7 +109,7 @@ DWORD __stdcall GameHandler::test(LPVOID param)
         {
             if (player->GetLocation().y <= 900) // 여기다가 하면 됨
             {
-                player->SetLocation(POINT{ player->GetLocation().x, player->GetLocation().y + 3 });
+                player->SetLocation(POINT{ player->GetLocation().x, player->GetLocation().y + 10 });
                 InvalidateRect(hWnd, NULL, FALSE);
             }
         }
@@ -121,11 +117,11 @@ DWORD __stdcall GameHandler::test(LPVOID param)
         {
             if (player->GetLocation().x <= 1080) // 여기다가 하면 됨
             {
-                player->SetLocation(POINT{ player->GetLocation().x + 3, player->GetLocation().y });
+                player->SetLocation(POINT{ player->GetLocation().x + 10, player->GetLocation().y });
                 InvalidateRect(hWnd, NULL, FALSE);
             }
         }
-        Sleep(20);
+        Sleep(30);
         //https://mlpworld.tistory.com/entry/키보드-상태-조사
     }
 
