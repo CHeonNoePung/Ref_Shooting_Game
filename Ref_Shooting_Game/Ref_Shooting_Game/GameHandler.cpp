5a94899ec4ccc648b2d17e02e81b71017c842696
202007055 <abc50050@gmail.com>
2022-02-13 22:14:30 +0900

    화면 크기 수정

--- Ref_Shooting_Game/Ref_Shooting_Game/GameHandler.cpp
+++ Ref_Shooting_Game/Ref_Shooting_Game/GameHandler.cpp
@@ -89,7 +89,7 @@ DWORD __stdcall GameHandler::test(LPVOID param)
 	{   // 해당 키가 눌리면 0x8000을 반환함 해당 키들을 계속 확인하면서 키가 눌렸는지 확인함
 		if (GetKeyState(0x57) & 0x8000) //w
 		{
-			if (player->GetLocation().y >= 10)
+			if (player->GetLocation().y >= 16)
 			{
 				player->SetLocation(POINT{ player->GetLocation().x, player->GetLocation().y - 10 });
 				InvalidateRect(hWnd, NULL, FALSE);
@@ -97,7 +97,7 @@ DWORD __stdcall GameHandler::test(LPVOID param)
 		}
 		if (GetKeyState(0x41) & 0x8000) //a
 		{
-			if (player->GetLocation().x >= 400)
+			if (player->GetLocation().x >= 406)
 			{
 				player->SetLocation(POINT{ player->GetLocation().x - 10, player->GetLocation().y });
 				InvalidateRect(hWnd, NULL, FALSE);
@@ -105,7 +105,7 @@ DWORD __stdcall GameHandler::test(LPVOID param)
 		}
 		if (GetKeyState(0x53) & 0x8000) //s
 		{
-			if (player->GetLocation().y <= 1020) // 여기다가 하면 됨
+			if (player->GetLocation().y <= 660) // 여기다가 하면 됨
 			{
 				player->SetLocation(POINT{ player->GetLocation().x, player->GetLocation().y + 10 });
 				InvalidateRect(hWnd, NULL, FALSE);
@@ -113,7 +113,7 @@ DWORD __stdcall GameHandler::test(LPVOID param)
 		}
 		if (GetKeyState(0x44) & 0x8000) //d
 		{
-			if (player->GetLocation().x <= 1000) // 여기다가 하면 됨
+			if (player->GetLocation().x <= 970) // 여기다가 하면 됨
 			{
 				player->SetLocation(POINT{ player->GetLocation().x + 10, player->GetLocation().y });
 				InvalidateRect(hWnd, NULL, FALSE);
--- Ref_Shooting_Game/Ref_Shooting_Game/Resource.h
+++ Ref_Shooting_Game/Ref_Shooting_Game/Resource.h
@@ -13,6 +13,10 @@
 #define IDC_REFSHOOTINGGAME             109
 #define IDR_MAINFRAME                   128
 #define IDB_BITMAP1                     129
+#define IDB_BITMAP2                     130
+#define IDB_BITMAP3                     131
+#define IDB_PNG1                        132
+#define IDB_BITMAP4                     141
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,7 +24,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        142
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
