25f46566565bdfaeaa3707302ddd8b6ddca84e07
202007051 <0j1129@naver.com>
2022-02-18 00:11:46 +0900

    사이즈

--- Ref_Shooting_Game/Ref_Shooting_Game/Ref_Shooting_Game.cpp
+++ Ref_Shooting_Game/Ref_Shooting_Game/Ref_Shooting_Game.cpp
@@ -114,7 +114,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
 	hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPED | WS_SYSMENU, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPED | WS_SYSMENU, CW_USEDEFAULT, 1425, 759, 0, nullptr, nullptr, hInstance, nullptr);
 
 	if (!hWnd)
 	{
